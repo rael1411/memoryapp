51e678e0b58bc60d3cafd547f58582d7248f68b0
Alessio <barrialex@gmail.com>
2021-05-24 09:36:20 +0200

    working solution, needs styling

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import "./Cards.css";
-import Score from "./Score/Score"
+import React, { useState, useEffect, useMemo} from "react"
+import "./Cards.css"
+import Score from "./Score/Score";
 import antimage from "../../assets/img/antimage_lg.png";
 import emberSpirit from "../../assets/img/ember_spirit_lg.png";
 import furion from "../../assets/img/furion_lg.png";
@@ -14,7 +14,8 @@ import stormSpirit from "../../assets/img/storm_spirit_lg.png";
 import windrunner from "../../assets/img/windrunner_lg.png";
 import wisp from "../../assets/img/wisp_lg.png";
 function Cards(props) {
-  const initialCards = [
+  
+  const initialCards = useMemo( () => [
     {
       url: antimage,
       clicked: false,
@@ -75,13 +76,15 @@ function Cards(props) {
       clicked: false,
       id: 11,
     },
-  ];
+  ],[]);
+  
   const [cards, setCards] = useState(initialCards);
   const [score, setScore] = useState(0);
   const [maxScore, setMaxScore] = useState(0);
-
   const shuffleFunction = (cards) => {
-    let tempArray = [...cards];
+    let tempArray = cards.map(card => {
+      return {...card};
+    })
     //fisher-yates shuffle
     let currentIndex = tempArray.length,
       temporaryValue,
@@ -99,29 +102,32 @@ function Cards(props) {
   //if a card has not been clicked, it flags it as clicked and updates score
   //otherwise resets the game
   const handleClick = (id) => {
-    let tempArray = [...cards];
+    let tempArray = cards.map(card => {
+      return {...card};
+    })
     let index = tempArray.findIndex((x) => x.id === id);
     if (!tempArray[index].clicked) {
       tempArray[index].clicked = true;
       setCards(tempArray);
-      setCards((card) => shuffleFunction(cards));
+      setCards(() => shuffleFunction(tempArray));
       setScore((score) => score + 1);
     } else {
-      setCards( shuffleFunction(initialCards));
+      setCards(shuffleFunction(initialCards));
       setScore(0);
     }
   };
-  //shuffles the cards the first t
+
+  //shuffles the cards the first time
   useEffect(() => {
     setCards((cards) => shuffleFunction(cards));
   }, []);
   //when the score updates, it checks/updates the max score
   //it also loops the game if all cards have been clicked
+  // eslint-disable-next-line
   useEffect(() => {
     score > maxScore && setMaxScore(score);
-    console.log(maxScore)
-    score % 12 === 0 && score !== 0 && setCards(shuffleFunction(initialCards))
-  }, [score, maxScore]);
+    score % 12 === 0 && score !== 0 && setCards(initialCards);
+  }, [score, maxScore, initialCards]);
 
   return (
     <div>
@@ -142,7 +148,7 @@ function Cards(props) {
           ))}
         </ul>
       </div>
-      <Score score={score} maxScore={maxScore}/>
+      <Score score={score} maxScore={maxScore} />
     </div>
   );
 }
